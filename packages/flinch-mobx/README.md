794ce3056429bb33be3c79df354fba8540e54381
geej <jeffrgee@gmail.com>
2019-11-02 22:55:03 -0700

    some documentation

---
+++ packages/flinch-mobx/README.md
@@ -0,0 +1,19 @@
+# flinch-mobx
+A simple higher-order component to trigger a state change when a MobX observable mutates.
+## Usage
+~~~~ 
+import { Observer } from '@flinch/flinch-mobx';
+import { observable } from 'mobx';
+import Flinch, { StatefulNode } from '@flinch/flinch-core';
+
+@Observer
+class ObservedNode extends StatefulNode {
+  @observable x = 0;
+
+   handleClick = () => this.x++;
+
+  render() {
+    return <button onClick={this.handleClick}>{ this.x }</button>;
+  }
+}
+~~~~
--- packages/flinch-mobx/index.js
+++ packages/flinch-mobx/index.js
@@ -3,11 +3,18 @@ import { observe } from 'mobx';
 
 /* @jsx Flinch.create */
 
+/**
+ * Observer wraps a StatefulNode, and triggers a Flinch state change when
+ * a MobX observable changes.
+ *
+ * @param {StatefulNode} Component - component to watch
+ */
 export function Observer(Component) {
   return class ObservedComponent extends StatefulNode {
     state = { mutator: 0 };
 
     componentDidMount() {
+      // TODO: This ref can change if the component's root node changes
       if (this.ref) {
         observe(this.ref, () => this.setState({ mutator: this.state.mutator + 1 }));
       }
