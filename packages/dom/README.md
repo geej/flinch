1af86458e3bf0e716c2e902a9e763e7d1abf0d04
geej <jeffrgee@gmail.com>
2019-11-20 23:26:56 -0800

    portal tests

--- packages/dom/README.md
+++ packages/dom/README.md
@@ -3,7 +3,7 @@ Adds support to Flinch for HTML tags and rendering onto the DOM.
 ## Usage
 ~~~~ 
 import Flinch from '@flinch/core';
-import { render } from '@flinch/dom';
+import render from '@flinch/dom';
 
 render(<div>hello!</div>, document.getElementById('root'));
 ~~~~
--- packages/portal/src/__test__/index.spec.js
+++ packages/portal/src/__test__/index.spec.js
@@ -1,5 +1,51 @@
 import PortalNode from '..';
+import HTMLNode from '@flinch/dom/dist/HTMLNode';
+import '@flinch/dom';
+import Flinch from '@flinch/core';
 
 describe('portal', () => {
-  test('dummy', () => {});
+  test('registers flinch type', () => {
+    const node = Flinch.create(PortalNode);
+    expect(node instanceof PortalNode).toBe(true);
+  });
+
+  test('portal node should draw to target idempotently', () => {
+    const target = document.createElement('div');
+    const innerNode = new HTMLNode('div');
+    const node = new PortalNode(null, { destination: target, children: innerNode });
+    node.update();
+    node.draw();
+
+    expect(target.childNodes[0] instanceof HTMLElement).toBe(true);
+
+    node.draw();
+    expect(target.childNodes.length).toBe(1);
+  });
+
+  test('portal should delete mountpoint on unmount', () => {
+    const target = document.createElement('div');
+    const innerNode = new HTMLNode('div');
+    const node = new PortalNode(null, { destination: target, children: innerNode });
+    node.update();
+    node.draw();
+
+    expect(target.childNodes.length).toBe(1);
+
+    node.unmount();
+
+    expect(target.childNodes.length).toBe(0);
+  });
+
+  test('portal should not error on unmount if not mounted', () => {
+    const target = document.createElement('div');
+    const innerNode = new HTMLNode('div');
+    const node = new PortalNode(null, { destination: target, children: innerNode });
+    node.update();
+
+    expect(target.childNodes.length).toBe(0);
+
+    node.unmount();
+
+    expect(target.childNodes.length).toBe(0);
+  });
 });
