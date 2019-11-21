96f9f9da19b58923fe18f2f0780e8ff68d66e6d1
geej <jeffrgee@gmail.com>
2019-11-20 21:01:27 -0800

    add more tests

--- packages/core/src/util/__test__/index.spec.js
+++ packages/core/src/util/__test__/index.spec.js
@@ -1,4 +1,5 @@
 import Util from '..';
+import Node from '../../Node';
 
 describe('Util', () => {
   describe('getFlatChildren', () => {
@@ -20,14 +21,41 @@ describe('Util', () => {
   });
 
   describe('cleanProps', () => {
+    test('should return undefined if no props', () => {
+      expect(Util.cleanProps()).toEqual(undefined);
+    });
 
+    test('should pluck undefined values', () => {
+      expect(Util.cleanProps({ a: undefined, b: 1 })).toEqual({ b: 1 });
+    });
   });
 
   describe('cloneNode', () => {
+    test('should copy node and retain prototype and values', () => {
+      const node = new Node();
+      node.setting = 'value';
+      const newNode = Util.cloneNode(node);
 
+      expect(node).not.toBe(newNode);
+      expect(newNode.setting).toEqual('value');
+      expect(newNode instanceof Node).toBe(true);
+    });
   });
 
   describe('findClosestAncestorWhere', () => {
+    const node1 = new Node();
+    const node2 = new Node();
+    const node3 = new Node();
+    node1.parent = node2;
+    node2.parent = node3;
+    node3.matches = true;
 
+    test('finds node that matches criteria', () => {
+      expect(Util.findClosestAncestorWhere(node1, node => node.matches)).toBe(node3);
+    });
+
+    test('returns undefined if no node matches criteria', () => {
+      expect(Util.findClosestAncestorWhere(node1, node => node.notAValue)).toBeFalsy();
+    });
   });
 });
